7668a06e9735b98a43f50d8100455703eb9f5dda
gulshanyadav258 <gulshanyadav258.com>
2021-07-04 15:16:57 +0530

    msg

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import { ProductsProvider } from './context/products_context'
+import { FilterProvider } from './context/filter_context'
+import { CartProvider } from './context/cart_context'
+import { UserProvider } from './context/user_context'
+import { Auth0Provider } from '@auth0/auth0-react'
+// dev-7al8owfu.us.auth0.com
+// 8BtJxHhUjgKRzseTdroBPh6dELd09wdL
+ReactDOM.render(
+  <Auth0Provider
+    domain='dev-7al8owfu.us.auth0.com'
+    clientId='8BtJxHhUjgKRzseTdroBPh6dELd09wdL'
+    redirectUri={window.location.origin}
+    cacheLocation='localstorage'
+  >
+    <UserProvider>
+      <ProductsProvider>
+        <FilterProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </FilterProvider>
+      </ProductsProvider>
+    </UserProvider>
+  </Auth0Provider>,
+
+  document.getElementById('root')
+)
